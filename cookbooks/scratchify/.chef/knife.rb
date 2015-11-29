e1df7e10fa4d129ad54fb215df37402b4e6857c4
Andrew Shaydurov <gearhead@it-primorye.ru>
2015-11-29 23:49:15 +1000

    [cleanup] removed unnecessary files from cookbook

--- cookbooks/scratchify/.chef/knife.rb
+++
@@ -1,6 +0,0 @@
-cookbook_path    "cookbooks"
-node_path        "tmp/nodes"
-data_bag_path    "tmp/data_bags"
-
-knife[:berkshelf_path] = "cookbooks"
-Chef::Config[:ssl_verify_mode] = :verify_peer if defined? ::Chef
--- cookbooks/scratchify/lib/from-scratch/version.rb
+++
@@ -1,3 +0,0 @@
-class FromScratch
-  VERSION = "0.6.0"
-end
